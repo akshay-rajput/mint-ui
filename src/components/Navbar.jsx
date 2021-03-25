0e1ef5cb9de2b80f74d277c58f96dcbb0385b8f5
Akshay <akshayrajput102@gmail.com>
2021-03-26 00:02:30 +0530

    Navbar - add logo and change colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <nav className="app-navbar pl4 pr4 md:pl8 md:pr8 pt3 pb3 bgGray6 textGray1">
+        <nav className="app-navbar pl4 pr4 md:pl8 md:pr8 pt3 pb3 bgGray5 textWhite">
             <div className=" displayFlex justifyBetween itemsCenter">
-                <a href="/" className="navbar-brand textTeal2 hover:textTeal4 textMd fontSemiBold h12 w12 p1 mr4">
-                    {/* <img src="https://www.pngitem.com/pimgs/m/359-3592112_icons-mint-linux-mint-start-menu-icons-hd.png" className="" alt="Mint Logo"/>  */}
-                    Mint
+                <a href="/" className="navbar-brand textGreen3 hover:textGreen5 textMd displayFlex itemsCenter fontSemiBold p1 mr4">
+                    <img src="./mint.png" className="w8" alt="Mint Logo"/> 
+                    <span className="ml1">Mint</span>
                 </a>
 
                 <ul className="nav-links displayFlex itemsCenter">
-                    <li className="textMd fontSemiBold textGray4 listNoStyle p2 mr4">
+                    <li className="textMd fontSemiBold textTeal4 listNoStyle p2 mr4">
                         v 0.1.0
                     </li>
-                    <li className="textMd fontSemiBold textGray4 listNoStyle p2 mr4">
+                    <li className="fontSemiBold textTeal4 listNoStyle p2 mr4">
                         <a href="https://github.com/akshay-rajput" className="">
-                            <i className="fab fa-github-alt textTeal2 hover:textTeal4"></i>
+                            <i className="fab fa-github-alt textTeal4 textLg hover:textTeal2"></i>
                         </a>
                     </li>
                     {/* <li className="textMd fontSemiBold textGray4 listNoStyle p2">
