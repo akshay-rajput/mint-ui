ab4ec23a46582de51f6c6a425b9880d59ff1b8d7
Akshay <akshayrajput102@gmail.com>
2021-03-25 17:15:07 +0530

    CodeDemo - added copy to clipboard button

--- src/components/CodeDemo.js
+++ src/components/CodeDemo.js
@@ -15,29 +15,41 @@ export default function CodeDemo({code, codeTitle, codeActions}) {
     function showPreview(isPreviewSelected){
         setPreview(isPreviewSelected)
     }
+    function copyText(){
+        navigator.clipboard.writeText(code).then(function() {
+            console.log('copied');
+        })
+    }
 
     return (
         
         <div className="code-demo mt2 mb2 border borderGray3">
-            <div className="code-actions displayFlex justifyBetween itemsCenter p2 pb1">
+            <div className="code-actions displayGrid gridCols2 itemsCenter p2 pb1">
                 <h4 className="textGray4">{codeTitle}</h4>
-                
-                {
-                    codeActions ? (
-                        <div className="btn-actions">
-                            <button onClick={()=> showPreview(true)} style={{background: previewEnabled?'': 'transparent'}} 
-                                    className="pl4 pr4 pt2 pb2 borderNone hover:bgGray3">
-                                <i className="far fa-eye mr1 textXs"></i>
-                                Preview
-                            </button>
-                            <button onClick={()=> showPreview(false)} style={{background: !previewEnabled?'': 'transparent'}} 
-                                    className="pl4 pr4 pt2 pb2 borderNone hover:bgGray3">
-                                <i className="fas fa-code mr1 textXs"></i>
-                                Code
-                            </button>
-                        </div>
-                    ): null
-                }
+                <div className="displayFlex flexWrap" style={{justifySelf: 'flex-end'}}>
+                    {
+                        codeActions ? (
+                            <div className="btn-actions">
+                                <button onClick={()=> showPreview(true)} style={{background: previewEnabled?'': 'transparent'}} 
+                                        className="pl4 pr4 pt2 pb2 borderNone hover:bgGray3">
+                                    <i className="far fa-eye mr1 textRg"></i>
+                                    {/* Preview */}
+                                </button>
+                                <button onClick={()=> showPreview(false)} style={{background: !previewEnabled?'': 'transparent'}} 
+                                        className="pl4 pr4 pt2 pb2 borderNone hover:bgGray3">
+                                    <i className="fas fa-code mr1 textRg"></i>
+                                    {/* Code */}
+                                </button>
+                            </div>
+                        ): null
+                    }
+                    <button onClick={copyText}  
+                            className="pl4 pr4 pt2 pb2 borderNone bgTransparent hover:bgGray3">
+                        <i className="far fa-copy mr1 textRg"></i>
+                        {/* Code */}
+                    </button>
+                </div>                
+
             </div>
 
             {
