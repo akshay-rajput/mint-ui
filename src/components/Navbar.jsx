ce2db7c7394eb0a3c361199e242748207ce72fde
Akshay <akshayrajput102@gmail.com>
2021-04-26 19:14:13 +0530

    style - navbar - update ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,17 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <nav className="app-navbar pl4 pr4 md:pl8 md:pr8 pt3 pb3 bgGray5 textWhite">
+        <nav className="app-navbar pl4 pr4 md:pl8 md:pr8 pt2 pb2 bgGray5 textWhite">
             <div className=" displayFlex justifyBetween itemsCenter">
                 <a href="/" className="navbar-brand textGreen3 hover:textGreen5 textMd displayFlex itemsCenter fontSemiBold p1 mr4">
-                    <img src="./mint.png" className="w8" alt="Mint Logo"/> 
-                    <span className="ml1">Mint</span>
+                    <img src="./mint.png" className="w6" alt="Mint Logo"/> 
+                    <span className="ml1 logofont">Mint</span>
                 </a>
 
                 <ul className="nav-links displayFlex itemsCenter">
-                    <li className="textMd fontSemiBold textTeal4 listNoStyle p2 mr4">
-                        v 0.1.0
-                    </li>
-                    <li className="fontSemiBold textTeal4 listNoStyle p2 mr4">
-                        <a href="https://github.com/akshay-rajput" className="">
-                            <i className="fab fa-github-alt textTeal4 textLg hover:textTeal2"></i>
+                    <li className="fontSemiBold textTeal4 listNoStyle p2">
+                        <a href="https://github.com/akshay-rajput/mint-ui" className="" title='View on Github'>
+                            <i className="fab fa-github textTeal4 textLg hover:textTeal2"></i>
                         </a>
                     </li>
                     {/* <li className="textMd fontSemiBold textGray4 listNoStyle p2">
